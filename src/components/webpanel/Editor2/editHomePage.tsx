894df889f27f2a3da3840bf542f5b2c56b3e62c0
nachodsang <nachodsang@gmail.com>
2023-07-05 10:54:32 +0700

    Update editHomePage.tsx

--- src/components/webpanel/Editor2/editHomePage.tsx
+++ src/components/webpanel/Editor2/editHomePage.tsx
@@ -16,7 +16,7 @@ const EditHomePage = () =>
   // { _model }: { _model: IHomePage }
   {
     // const [model, setModel] = useState<IHomePage>(_model != null ? _model : { title: '', title2: '', group: [] })
-    const [model, setModel] = useState<IGroup>({ grid: [] });
+    const [model, setModel] = useState<IGroup>({ grid: [{ contents: [] }] });
     const [modelAdd, setModelAdd] = useState<IGrid>();
     const [modelEditor, setModelEditor] = useState<any>();
     const [modelEditorContent, setModelEditorContent] = useState<any>();
@@ -50,7 +50,7 @@ const EditHomePage = () =>
         if (_model) {
           _model.grid?.push(modelAdd);
           setModel(_model);
-          // console.log(model);
+          console.log(model);
         }
       }
     }, [modelAdd]);
@@ -136,6 +136,17 @@ const EditHomePage = () =>
     return (
       <div>
         {/* {JSON.stringify(modelEditor)} */}
+        <button
+          type="button"
+          data-te-toggle="modal"
+          data-te-target="#addModel"
+          data-te-ripple-init
+          data-te-ripple-color="light"
+          className="hover:bg-primary-600  focus:bg-primary-600 active:bg-primary-700 m-1 inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+          onClick={() => {}}
+        >
+          Add
+        </button>
 
         {model
           ? model.grid.map((itemGrid: IGrid, indexGrid: number) => (
